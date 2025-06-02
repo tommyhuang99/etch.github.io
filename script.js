7a2bab0b57b05343a4bd1edc036c98fd66fbc343
tommyhuang99 <minchiahuang99@gmail.com>
2025-06-02 15:27:40 +1000

    1.0.2 feat: add new color button

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+let currentColor = "#000000";
+let isDrawing = false;
 
-
+const colorBtn = document.getElementById('btC');
+const colorPicker = document.getElementById('colorPicker');
 const container = document.querySelector('.container')
+
+colorBtn.addEventListener('click', () => {
+  colorPicker.click();
+})
+
+colorPicker.addEventListener('input', (e) => {
+  currentColor = e.target.value;
+});
+
+document.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+document.addEventListener("mouseup", () => {
+  isDrawing = false;
+})
+
 for (let i = 0; i < 256; i++) {
   const square = document.createElement('div');
   square.classList.add('grid-square');
   container.appendChild(square);
-  square.addEventListener("mouseover", function () {
-    square.style.backgroundColor = "black";
+
+  square.addEventListener("mousedown", function () {
+    square.style.backgroundColor = currentColor;
+  });
+
+  square.addEventListener("mousemove", function () {
+    if (isDrawing) {
+      square.style.backgroundColor = currentColor;
+    }
   });
 }
 
@@ -14,14 +41,14 @@ for (let i = 0; i < 256; i++) {
 document.getElementById("btN").addEventListener("click", newGrid)
 
 function newGrid() {
-  let inout = prompt("Please enter a new grid size (1–100):");
+  let input = prompt("Please enter a new grid size (1–100):");
 
   const newGridSize = parseInt(input);
 
   if (isNaN(newGridSize)) {
     alert("Please enter the valid number");
     return;
-  } else if (newGridSize > 1 || newGridSize > 100) {
+  } else if (newGridSize < 1 || newGridSize > 100) {
     alert("Please enter the number between 1 to 100");
     return;
   }
@@ -29,6 +56,15 @@ function newGrid() {
 
   let squares = document.querySelectorAll('.grid-square');
   for (let i = 0; i < squares.length; i++) {
-    squares[i] = element.remove();
+    squares[i].remove();
+  }
+
+  const container = document.querySelector('.container');
+  container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
+
+  for (let i = 0; i < newGridSize * newGridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    container.appendChild(square);
   }
 }
