4b70a8b8f21ea31ac014b7cbb7c99f674e482eba
tommyhuang99 <minchiahuang99@gmail.com>
2025-06-02 17:29:12 +1000

    1.0.3 rewrite js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let currentColor = "#000000";
-let isDrawing = false;
+//let isDrawing = false;
 
 const colorBtn = document.getElementById('btC');
 const colorPicker = document.getElementById('colorPicker');
 const container = document.querySelector('.container')
 
+
+//color picker functionality
 colorBtn.addEventListener('click', () => {
   colorPicker.click();
 })
@@ -13,58 +15,54 @@ colorPicker.addEventListener('input', (e) => {
   currentColor = e.target.value;
 });
 
-document.addEventListener("mousedown", () => {
-  isDrawing = true;
-});
 
-document.addEventListener("mouseup", () => {
-  isDrawing = false;
-})
+//Function to create grid with specified size
+function createGrid(gridSize) {
+  container.innerHTML = '';
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.classList.add('grid-square');
-  container.appendChild(square);
+  const squareSize = 960 / gridSize;
 
-  square.addEventListener("mousedown", function () {
-    square.style.backgroundColor = currentColor;
-  });
-
-  square.addEventListener("mousemove", function () {
-    if (isDrawing) {
-      square.style.backgroundColor = currentColor;
-    }
-  });
-}
-
-
-document.getElementById("btN").addEventListener("click", newGrid)
-
-function newGrid() {
-  let input = prompt("Please enter a new grid size (1–100):");
-
-  const newGridSize = parseInt(input);
-
-  if (isNaN(newGridSize)) {
-    alert("Please enter the valid number");
-    return;
-  } else if (newGridSize < 1 || newGridSize > 100) {
-    alert("Please enter the number between 1 to 100");
-    return;
-  }
-  console.log(`Create new Grid: ${newGridSize} x ${newGridSize}`);
-
-  let squares = document.querySelectorAll('.grid-square');
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].remove();
-  }
-
-  const container = document.querySelector('.container');
-  container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
-
-  for (let i = 0; i < newGridSize * newGridSize; i++) {
-    const square = document.createElement('div');
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement('div')
     square.classList.add('grid-square');
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
+    //hover effect
+    square.addEventListener('mouseenter', function () {
+      this.style.background = currentColor;
+    });
+
     container.appendChild(square);
   }
 }
+
+createGrid(16);
+
+document.getElementById('btN').addEventListener("click", function () {
+  let input = prompt("Please enter a new grid size(1-100):");
+
+  const newGridSize = parseint(input);
+
+  if (isNaN(newGridSize)) {
+    alert("Please enter a valid number");
+    return;
+  } else if (newGridSize < 1 || newGridSize > 100) {
+    alert("Please enter a number between 1 to 100");
+    return;
+  }
+
+  createGrid(newGridSize);
+});
+
+
+// Rest button
+
+document.getElementById("btR").addEventListener("click", function () {
+  const square = document.querySelectorAll('.grid-square');
+  square.forEach(square => {
+    square.style.background = '';
+  });
+});
+
